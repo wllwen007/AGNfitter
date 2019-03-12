8eb1f029280aa5bafb3e99e849275988ead48afe
Wendy Williams <wllwen007@gmail.com>
2019-03-12 10:41:16 +0000

    add option to give file containing filters

--- functions/DICTIONARIES_AGNfitter.py
+++ functions/DICTIONARIES_AGNfitter.py
@@ -16,6 +16,7 @@ This script contains all functions which are needed to construct the total model
 """
 import numpy as np
 import sys
+import os
 from collections import defaultdict
 
 import MODEL_AGNfitter as model
@@ -251,12 +252,13 @@ def filter_dictionaries(filterset, path, filters):
 
     input
     -------
-    - filterset: Here we have two types of filterset: 
-    'BANDSET_default' or 'BANDSET_settings'.
+    - filterset: Here we have three types of filterset: 
+    'BANDSET_default' or 'BANDSET_settings' or 'BANDSET_file'.
     This was specified from the RUN_AGNfitter_multi.py script.
 
     'BANDSET_default' includes bands needed for the example.
     'BANDSET_settings' includes all bands you specify in RUN_AGNfitter_multi.py. 
+    'BANDSET_file' specify a filename contaiing the full paths for your set of filters
 
     dependency
     ----------
@@ -472,7 +474,7 @@ def filter_dictionaries(filterset, path, filters):
             central_nu_list.append(central_nu)
 
 
-    if filterset == 'BANDSET_settings':
+    elif filterset == 'BANDSET_settings':
 
         files =[]
         lambdas = []
@@ -717,6 +719,66 @@ def filter_dictionaries(filterset, path, filters):
             #print i, central_nu
 
         central_nu_list = sorted(central_nu_list)
+            
+            
+            
+    elif filterset == 'BANDSET_file':
+        
+        
+
+        files =[]
+        lambdas = []
+        factors = []
+
+        if filters['file']:
+            
+            if not os.path.exists(filters['file']):
+                print 'Error: specified filter file list', filters['file'],' does not exist'
+                sys.exit(1)
+                
+            with open(filters['file'],'r') as f:
+                filterfiles = f.readlines()
+                filterfiles = [f.strip() for f in filterfiles]
+            
+            for band_file in filterfiles:
+                print 'Reading filterfile:', band_file
+                
+                #band_file = path + 'models/FILTERS/HERSCHEL/SPIRE_500mu.txt'
+                band_lambda, band_factor =  np.loadtxt(H500band_file, usecols=(0,1),unpack= True)
+
+                files.append(band_file)
+                lambdas.append(band_lambda)
+                factors.append(band_factor)
+            
+
+        # make dictionaries lambdas_dict, factors_dict
+        files_dict = defaultdict(list)
+        lambdas_dict = defaultdict(list)
+        factors_dict = defaultdict(list)
+        central_nu_list=[]        
+
+        #print len(files)
+        for i in range(len(files)):
+
+            c=    2.997e8
+            Angstrom = 1e10
+
+            central_lamb = np.sum(lambdas[i]*factors[i])/np.sum(factors[i])
+            central_nu = float(np.log10((Angstrom*c)/central_lamb))
+
+            files_dict[central_nu].append(files[i])
+            lambdas_dict[central_nu].append(lambdas[i])
+            factors_dict[central_nu].append(factors[i])
+
+            central_nu_list.append(central_nu)
+            
+            #print i, central_nu
+
+        central_nu_list = sorted(central_nu_list)
+        
+    else:
+        print  'Error: filterset', filterset, 'not handled'
+        sys.exit(1)
             
     return np.array(central_nu_list), files_dict, lambdas_dict, factors_dict
 
